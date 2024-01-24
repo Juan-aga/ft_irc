17d5458006222ca6c840c18a1bbf6f1a0271bd1c
Pedro Reina Ganan <preina-g@c2r5s6.42malaga.com>
2024-01-24 15:30:53 +0100

    poll made and fixed

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -23,7 +23,7 @@ class Server
 		std::string _password;
 		int 		_port;
         int         _socket_fd;
-		//int			_clientFd;
+		int			_clientFd;
 		Commands	_commands;
 		Client		_client;
 
@@ -36,4 +36,6 @@ class Server
 
         void createSocket(void);
         void connectClient(void);
+		void readMesage(int &clientFd);
+		void newClient(std::vector<struct pollfd> &pollfds);
 };
